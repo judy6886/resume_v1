8f27c6d9c2f7530e57bdbd77148c583f15590fd0
judy6886 <samantha_judy@163.com>
2017-03-07 11:45:23 +0800

    修复百度API

---
+++ js/baidu_api.js
@@ -0,0 +1,1 @@
+(function(){ window.BMap_loadScriptTime = (new Date).getTime(); document.write('<script type="text/javascript" src="http://api.map.baidu.com/getscript?v=2.0&ak=EAc9690e060a06cfe50a260d491afd15&services=&t=20170220193356"></script>');})();
