b501162edee367b320e84cf249643456c69cf485
judy6886 <samantha_judy@163.com>
2017-03-08 01:01:31 +0800

    再次修复

--- js/baiduapi.js
+++ js/baiduapi.js
@@ -1,4 +1,4 @@
 (function(){ 
 	window.BMap_loadScriptTime = (new Date).getTime();
-	document.write('<script type="text/javascript" src="https://api.map.baidu.com/getscript?v=2.0&ak=EAc9690e060a06cfe50a260d491afd15&services=&t=20170220193356"></script>');
+	document.write('<script type="text/javascript" src="https://api.map.baidu.com/getscript?v=2.0&ak=EAc9690e060a06cfe50a260d491afd15&services=&t=20170220193356&s=1"></script>');
 })();
